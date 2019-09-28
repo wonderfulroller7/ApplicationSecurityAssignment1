36e0814d4f5f27719543e5914c66043b968e656d
wonderfulroller7 <sb6856@nyu.edu>
2019-09-28 12:29:06 -0400

    Implementing load_dictionary in spell file

--- spell.c
+++ spell.c
@@ -1,7 +1,30 @@
 #include<stdlib.h>
+#include<stdio.h>
 #include "dictionary.h"
 
-bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]){
+int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[]) {
 
-	return true;
+	int result = EXIT_SUCCESS;
+	return result;
+}
+
+bool check_word(const char* word, hashmap_t hashtable[]) {
+
+	bool valid_word = true;
+	
+	return valid_word;
+}
+
+bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]) {
+
+	bool load = true;
+	printf("%s\n", dictionary_file);
+	FILE *dictionary = fopen(dictionary_file, "r");
+	if ( dictionary == NULL ) {
+		perror( "Dictionary file error" );
+		load = false;
+	} else {
+		printf("Dictionary file loading is a success\n");
+	}
+	return load;
 }
